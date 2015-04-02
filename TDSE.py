05b368c2baadb6ca88603a77b69baf3f62740951
Jackson <jacksonburzynski@gmail.com>
2015-04-02 16:15:14 -0400

    added plotting capability to check that the code works

--- TDSE.py
+++ TDSE.py
@@ -69,7 +69,8 @@ def init_psi(a,numX,deltaX):
 
 # constructs the initial wave function
 def wavePacket(x):
-	return cmath.cos((np.pi * x)/10)
+	return cmath.exp(-(x)**2)
+#	return cmath.cos((np.pi * x)/10)
 
 # run parameters
 a=-5
@@ -95,9 +96,33 @@ mat = buildMatrix(numX,numT,V,deltaX,deltaT)
 # solve for psi
 psi = finiteDifference(numX, numT, deltaX, deltaT, mat, psi_init)
 
+
+prob_density = np.zeros((numT,numX))
+
+for n in range(numT):
+	for i in range(numX):
+		prob_density[n,i] = abs(psi[n,i])**2
+
 #for i in range(numT):
 #	print psi[i,:]
 
 
-plt.plot(abs(psi[5,:])**2)
+#plt.plot(abs(psi[5,:])**2)
+#plt.show()
+
+#H = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]) 
+
+fig = plt.figure(figsize=(6, 3.2))
+
+ax = fig.add_subplot(111)
+ax.set_title('colorMap')
+plt.imshow(prob_density)
+ax.set_aspect('equal')
+
+cax = fig.add_axes([0.12, 0.1, 0.78, 0.8])
+cax.get_xaxis().set_visible(False)
+cax.get_yaxis().set_visible(False)
+cax.patch.set_alpha(0)
+cax.set_frame_on(False)
+plt.colorbar(orientation='vertical')
 plt.show()
