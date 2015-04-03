99cdb8b0ab16736e45117c5916ad58b12cb8d99e
Caleb Helbling <caleb.helbling@gmail.com>
2015-04-03 19:00:30 -0400

    Added new potentials and wave functions

--- Main.py
+++ Main.py
@@ -7,8 +7,8 @@ import Visualize
 xs = Range(100, 10)
 ts = Range(500, 20)
 
-potential = Potential.init(xs, xs.center_function(Potential.harmonic_oscillator))
-psi_init = WaveFunction.init(xs, xs.center_function(WaveFunction.wave_packet))
+potential = Potential.init(xs, xs.center_function(Potential.barrier(0.5, 0.5)))
+psi_init = WaveFunction.init(xs, xs.center_function(WaveFunction.gaussian_wave(7.0)))
 
 psi = FiniteDifference.solve(xs, ts, potential, psi_init)
 Visualize.heatmap(psi)
--- Potential.py
+++ Potential.py
@@ -1,5 +1,10 @@
+import numpy as np
+
 def init(xs, potential_fun):
-    return potential_fun(xs.get_values())
+    return np.vectorize(potential_fun)(xs.get_values())
 
 def harmonic_oscillator(x):
     return .5 * x**2
+
+def barrier(width, height):
+    return lambda x : height if abs(x) <= (width / 2.0) else 0.0
