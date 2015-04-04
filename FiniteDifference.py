448bd4de762d0ffb2d52043ad60f89412d3eb126
Jackson <jacksonburzynski@gmail.com>
2015-04-03 20:05:31 -0400

    implemented second algorithm

--- FiniteDifference.py
+++ FiniteDifference.py
@@ -1,9 +1,14 @@
 import numpy as np
 import WaveFunction
 
-def solve(xs, ts, potential, psi_init):
-    matrix = build_matrix(xs, ts, potential)
-    return finite_difference(xs, ts, psi_init, matrix)
+def solve(xs, ts, potential, psi_init, algorithm):
+	if algorithm == 1:
+		matrix = build_matrix(xs, ts, potential)
+		return finite_difference1(xs, ts, psi_init, matrix)
+	else:
+		left_matrix = build_matrix_left(xs, ts, potential)
+		right_matrix = build_matrix_right(xs, ts, potential)
+		return finite_difference2(xs, ts, psi_init, left_matrix, right_matrix)
 
 def build_matrix(xs, ts, potential):
     num_x = xs.get_num_divisions()
@@ -64,4 +69,21 @@ def finite_difference(xs, ts, psi_init, matrix):
         psi[n+1,0] = 0
         psi[n+1,-1] = 0
         psi[n+1,:] = WaveFunction.normalize(psi[n+1,:])
+    return psi
+
+def finite_difference2(xs, ts, psi_init, left_matrix, right_matrix):
+    num_x = xs.get_num_divisions()
+    num_t = ts.get_num_divisions()
+    delta_t = ts.get_delta()
+    psi = np.zeros((num_t,num_x), dtype=complex)
+    psi[0,:] = psi_init
+    # boundary conditions
+    psi[:,0] = 0
+    psi[:,-1] = 0
+    for n in xrange(num_t-1):
+        b = psi[n,:]
+        psi[n+1,:] = np.linalg.solve(left_matrix,np.dot(right_matrix,b))
+        psi[n+1,0] = 0
+        psi[n+1,-1] = 0
+#        psi[n+1,:] = WaveFunction.normalize(psi[n+1,:])
     return psi
