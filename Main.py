df06e7753e285c66a35285e0e3ec9a1a81b3eb6c
Jackson <jacksonburzynski@gmail.com>
2015-04-04 01:22:37 -0400

    minor changes

--- Main.py
+++ Main.py
@@ -5,10 +5,10 @@ import FiniteDifference
 import Visualize
 
 xs = Range(200, 20)
-ts = Range(1000, 10)
+ts = Range(1000, 40)
 
 potential = Potential.init(xs, xs.center_function(Potential.infinite_well()))
-psi_init = WaveFunction.init(xs, xs.center_function(WaveFunction.traveling_wave(2,-5.0)))
+psi_init = WaveFunction.init(xs, xs.center_function(WaveFunction.gaussian_wave(0.0)))
 
 psi = FiniteDifference.solve(xs, ts, potential, psi_init, 2)
 Visualize.animate_wave(xs,ts,psi)
